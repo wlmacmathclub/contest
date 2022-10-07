b0a8cafb1170982753ed2d39b631e6bb6cf6d3a5
Patrick Lin <apocalypsecalculator@gmail.com>
2022-10-07 16:14:52 -0400

    generate prettier pdf :tm:

--- genpdf.go
+++ genpdf.go
@@ -13,74 +13,160 @@ func generatePDF(user User, contest Contest) {
 	begin := time.Now()
 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 	m.SetPageMargins(10, 15, 10)
+	m.SetDefaultFontFamily(consts.Courier)
 
 	m.SetAliasNbPages("{nb}")
 	m.SetFirstPageNb(1)
 
 	m.RegisterHeader(func() {
-		m.Row(22, func() {
+		m.Row(40, func() {
 			m.Col(0, func() {
 				m.Text("Math Contest Permission Form", props.Text{
-					Size:  15,
+					Size:  20,
 					Style: consts.Bold,
 					Align: consts.Center,
 					Top:   4,
 				})
 				m.Text("William Lyon Mackenzie C.I.", props.Text{
-					Size:  14,
+					Size:  17,
 					Align: consts.Center,
-					Top:   12,
+					Top:   20,
 				})
 			})
 		})
 
-		m.Line(1.0, props.Line{
+		m.Line(2, props.Line{
 			Style: consts.Solid,
 			Width: 0.5,
 		})
 	})
 
+	m.RegisterFooter(func() {
+		m.Row(20, func() {
+			m.Col(0, func() {
+				m.Text("Registration system created by Patrick Lin", props.Text{
+					Size:  12,
+					Align: consts.Center,
+					Top:   4,
+				})
+			})
+		})
+	})
+
+	m.Row(40, func() {
+		m.Col(6, func() {
+			m.Text("Student Name: ", props.Text{
+				Top:   20,
+				Size:  15,
+				Align: consts.Right,
+				Style: consts.Bold,
+			})
+		})
+		m.Col(6, func() {
+			m.Text(fmt.Sprintf("%s %s", user.firstName, user.lastName), props.Text{
+				Top:   20,
+				Size:  15,
+				Align: consts.Left,
+			})
+		})
+	})
 	m.Row(20, func() {
-		m.Col(12, func() {
-			m.Text(fmt.Sprintf("Student Name: %s %s", user.firstName, user.lastName), props.Text{
-				Top:   10,
-				Size:  12,
-				Align: consts.Center,
+		m.Col(6, func() {
+			m.Text("Contest Name: ", props.Text{
+				Size:  15,
+				Align: consts.Right,
+				Style: consts.Bold,
+			})
+		})
+		m.Col(6, func() {
+			m.Text(contest.name, props.Text{
+				Size:  15,
+				Align: consts.Left,
 			})
 		})
 	})
-	m.Row(10, func() {
-		m.Col(12, func() {
-			m.Text(fmt.Sprintf("Contest Name: %s", contest.name), props.Text{
-				Size:  12,
-				Align: consts.Center,
+	m.Row(20, func() {
+		m.Col(6, func() {
+			m.Text("Contest Date: ", props.Text{
+				Size:  15,
+				Align: consts.Right,
+				Style: consts.Bold,
+			})
+		})
+		m.Col(6, func() {
+			m.Text(contest.date.Format(time.RFC822), props.Text{
+				Size:  15,
+				Align: consts.Left,
 			})
 		})
 	})
-	m.Row(10, func() {
-		m.Col(12, func() {
-			m.Text(fmt.Sprintf("Contest Date: %s", contest.date.Format(time.RFC822)), props.Text{
-				Size:  12,
-				Align: consts.Center,
+	m.Row(20, func() {
+		m.Col(6, func() {
+			m.Text("Period 1 Teacher: ", props.Text{
+				Size:  15,
+				Align: consts.Right,
+				Style: consts.Bold,
+			})
+		})
+		m.Col(6, func() {
+			m.Text(user.firstTeacher, props.Text{
+				Size:  15,
+				Align: consts.Left,
 			})
 		})
 	})
+	if user.secondTeacher != "" {
+		m.Row(20, func() {
+			m.Col(6, func() {
+				m.Text("Period 2 Teacher: ", props.Text{
+					Size:  15,
+					Align: consts.Right,
+					Style: consts.Bold,
+				})
+			})
+			m.Col(6, func() {
+				m.Text(user.secondTeacher, props.Text{
+					Size:  15,
+					Align: consts.Left,
+				})
+			})
+		})
+	}
 
 	m.Row(20, func() {
 		if user.secondTeacher == "" {
 			m.Col(3, func() {})
 			m.Col(6, func() {
-				m.Signature(fmt.Sprintf("Signature of %s (Period 1)", user.firstTeacher))
+				m.Signature("Period 1 Teacher Signature", props.Font{
+					Style: consts.Normal,
+					Size:  15,
+				})
 			})
 			m.Col(3, func() {})
 		} else {
 			m.Col(6, func() {
-				m.Signature(fmt.Sprintf("Signature of %s (Period 1)", user.firstTeacher))
+				m.Signature("Period 1 Teacher Signature", props.Font{
+					Style: consts.Normal,
+					Size:  15,
+				})
 			})
 			m.Col(6, func() {
-				m.Signature(fmt.Sprintf("Signature of %s (Period 2)", user.secondTeacher))
+				m.Signature("Period 2 Teacher Signature", props.Font{
+					Style: consts.Normal,
+					Size:  15,
+				})
 			})
 		}
+	})
+
+	m.Row(40, func() {
+		m.Col(0, func() {
+			m.Text("If you do not show up to the contest you may penalized!", props.Text{
+				Top:   30,
+				Size:  13,
+				Align: consts.Center,
+			})
+		})
 	})
 
 	m.OutputFileAndClose(fmt.Sprintf("cache/%s_%s_%s_%d%d%d.pdf", contest.name, user.firstName, user.lastName, contest.date.Year(), contest.date.Month(), contest.date.Day()))
