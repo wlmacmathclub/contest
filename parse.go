3863ac21d2acaf309e31c549076655a897750346
Patrick Lin <apocalypsecalculator@gmail.com>
2022-10-07 01:43:32 -0400

    consider no P2 scenario

--- parse.go
+++ parse.go
@@ -18,13 +18,19 @@ func parseCSV(path string) []User {
 
 	users := make([]User, 0)
 	for _, person := range recs {
-		if len(person) >= 5 {
+		if len(person) >= 4 {
 			users = append(users, User{
-				email:         person[0],
-				firstName:     person[1],
-				lastName:      person[2],
-				firstTeacher:  person[3],
-				secondTeacher: person[4],
+				email:        person[0],
+				firstName:    person[1],
+				lastName:     person[2],
+				firstTeacher: person[3],
+				secondTeacher: func() string {
+					if len(person) > 4 {
+						return person[4]
+					} else {
+						return ""
+					}
+				}(),
 			})
 		}
 	}
