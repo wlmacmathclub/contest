6880885c0158d82119bed241551e16be94890b91
Patrick Lin <apocalypsecalculator@gmail.com>
2022-10-07 01:30:57 -0400

    temp sample commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+func main() {
+	contestApp := app.New()
+	appWindow := contestApp.NewWindow("Grid Layout")
+	appWindow.Resize(fyne.NewSize(1280, 720))
+
+	text1 := canvas.NewText("1", color.White)
+	text2 := canvas.NewText("2", color.White)
+	text3 := canvas.NewText("3", color.White)
+	grid := container.New(layout.NewGridLayout(2), text1, text2, text3)
+	appWindow.SetContent(grid)
+	appWindow.ShowAndRun()
+}
